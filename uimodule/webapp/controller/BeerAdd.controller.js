6112b4a76fc68c15ed1b8c7916666d583ccf4824
DavidMyan <iloja@mail.ru>
2024-01-19 13:18:47 +0400

    Navigtion & Models

---
+++ uimodule/webapp/controller/BeerAdd.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/apress/openui5/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.apress.openui5.controller.BeerAdd", {});
+});
